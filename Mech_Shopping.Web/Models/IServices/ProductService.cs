d3347e939b1d0aef983ab86bfa787b8fab7eb141
viniciusbassio <viniciusbassioa11@gmail.com>
2025-10-11 15:58:05 -0300

    Feat:
    Integração inicial do front-end com a Product API
    
    - Adicionada pasta `Utils` ao projeto `MechShopping.ProductAPI`.
    - Ajustes na migração `20250930233336_Inicial.cs` (formatação).
    - Inserção de dados iniciais na tabela `Product` em `SQLServerContext`.
    - Configurado `HttpClient` para `IProductService` em `Program.cs`.
    - Adicionada configuração `ServiceUrls:ProductAPI` em `appsettings.json`.
    - Criada classe `ProductModel` para representar produtos.
    - Criada interface `IProductService` e implementação em `ProductService`.
    - Adicionados métodos de extensão para `HttpClient` em `HttpClientExtensions`.

---
+++ Mech_Shopping.Web/Models/IServices/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using Mech_Shopping.Web.Utils;
+
+namespace Mech_Shopping.Web.Models.IServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient client;
+        
+        public const string BasePath = "api/v1/products";
+        public ProductService(HttpClient httpClient)
+        {
+            client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+        public async Task<ProductModel> CreateProduct(ProductModel productModel)
+        {
+            var response = await client.PostAsJson(BasePath, productModel);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+        public async Task<ProductModel> UpdateProduct(ProductModel productModel)
+        {
+            var response = await client.PutAsJson($"{BasePath}/{productModel.Id}", productModel);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await client.DeleteAsync($"{BasePath}/{id}");
+            return response.IsSuccessStatusCode;
+        }
+
+
+        
+    }
+}
